ec5efa5ae16ddb2847a8f8913578ab2f65c013d8
Klau <klaudiusmiskinis@gmail.com>
2021-10-13 17:54:52 +0200

    Creando el componente heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Contador } from './contador/contador.component';
+import { Heroes } from './Heroes/Heroe/heroe.component';
+import { HeroeComponent } from './Heroes/Heroe/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
