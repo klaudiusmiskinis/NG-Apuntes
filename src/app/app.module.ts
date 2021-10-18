be14924c133cabcdf992e7f8eaf4315a1c91273e
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-18 23:10:08 +0200

    Editando los imports y eliminando AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { Heroes } from './Heroes/Heroe/heroe.component';
-import { HeroeComponent } from './Heroes/Heroe/heroe/heroe.component';
+// Componentes
+import { HeroeComponent } from './Heroes/Heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     HeroeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HeroeComponent]
 })
 export class AppModule {  }
