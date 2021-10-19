6162d4fddfbc61f90c1f4dded3945cde57cafc3a
Klau <klaudiusmiskinis@gmail.com>
2021-10-19 17:49:40 +0200

    Eliminando contador y coches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeroeComponent } from './Heroes/Heroe/heroe.component';
 import { ListadoComponent } from './Heroes/listado/listado.component';
-import { ContadorModule } from './contador/contador.module';
-
 
 @NgModule({
   declarations: [
